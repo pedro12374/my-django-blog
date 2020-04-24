560f9c468c6141450c0d1a6fec37882b64b7bd5d
pedro12374 <pedrohaerter.095@gmail.com>
2020-04-24 19:30:12 -0300

    New HTLM and Data

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank = True,null = True)
-
+    pi = 3.141592
     def publish(self):
         self.published_date = timezone.now()
         self.save()
